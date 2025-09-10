4c3551272726fe618c655afbe83d33fab8088397
voidiz <29259387+voidiz@users.noreply.github.com>
2025-09-10 20:28:55 +0200

    default to info log level

--- crates/seedmirror-client/src/main.rs
+++ crates/seedmirror-client/src/main.rs
@@ -10,7 +10,7 @@ mod workqueue;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    env_logger::init();
+    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
     let args = cli::Args::parse();
 
     let queue = Workqueue::new();
--- crates/seedmirror-server/src/main.rs
+++ crates/seedmirror-server/src/main.rs
@@ -8,7 +8,7 @@ mod watcher;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    env_logger::init();
+    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
     let args = cli::Args::parse();
 
     let mut set = JoinSet::new();
