1c2784d87037d9da64923ab354e6b34444592dcd
voidiz <29259387+voidiz@users.noreply.github.com>
2026-08-05 20:47:28 +0000

    fix gui scroll overflow

--- crates/seedmirror-client/src/frontend/src/pages/index.tsx
+++ crates/seedmirror-client/src/frontend/src/pages/index.tsx
@@ -2,7 +2,7 @@ import { SyncInfo } from "@/components/sync-info";
 
 export function Index() {
   return (
-    <div className="h-full w-full flex items-center justify-center overflow-auto">
+    <div className="w-full flex items-center justify-center overflow-auto">
       <SyncInfo />
     </div>
   );
